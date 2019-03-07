29d7bac857dfed3d8b2952fba4aa5759134fa1df
Hector <hfierroprog@gmail.com>
2019-03-07 00:56:28 -0300

    Se cambia a motor de base de datos h2, de esta manera es llegar y correr el aplicativo

--- src/main/resources/data.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO tarea(texto,estado) values ('Tarea 1', 'POR COMPLETAR');
-INSERT INTO tarea(texto,estado) values ('Tarea 2', 'COMPLETADA');
-INSERT INTO tarea(texto,estado) values ('Tarea 3', 'POR COMPLETAR');
---
+++ src/main/resources/import.sql
@@ -0,0 +1,3 @@
+INSERT INTO tarea(texto,estado) values ('Hacer Deporte', 'POR COMPLETAR');
+INSERT INTO tarea(texto,estado) values ('Comprar Utiles De Aseo', 'COMPLETADA');
+INSERT INTO tarea(texto,estado) values ('Hora de Lectura', 'POR COMPLETAR');
